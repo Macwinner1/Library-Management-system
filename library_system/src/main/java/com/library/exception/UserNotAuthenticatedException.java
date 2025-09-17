59e1667cb7162a1c94d5d41250355bdeece1b0ac
Macwinner1 <ifechukwuw@gmail.com>
2025-09-17 04:07:13 +0100

    exceptions handling classes

---
+++ library_system/src/main/java/com/library/exception/UserNotAuthenticatedException.java
@@ -0,0 +1,11 @@
+package main.java.com.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class UserNotAuthenticatedException extends RuntimeException {
+    public UserNotAuthenticatedException(String message) {
+        super(message);
+    }
+}
